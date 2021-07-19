b2a0c4903d4056acb091cd173608b6658dbc58f2
Simpolor <simpolor@gmail.com>
2021-07-19 23:47:49 +0900

    .

--- api/src/main/java/io/simpolor/api/advice/ServiceExceptionHandler.java
+++ api/src/main/java/io/simpolor/api/advice/ServiceExceptionHandler.java
@@ -5,6 +5,7 @@ import io.simpolor.api.exception.ServiceException;
 import io.simpolor.api.exception.WrongApiUsageException;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -12,9 +13,14 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+/***
+ * @RestControllerAdvice는 @ControllerAdvice+@ResponseBody의 역할을 한다.
+ * @ControllerAdvice를 사용할 경우 ResponseEntity를 통해 status와 body를 지정해야 한다.
+ */
 @Slf4j
 @RestControllerAdvice(annotations = WithServiceResponse.class)
 public class ServiceExceptionHandler {
@@ -28,6 +34,12 @@ public class ServiceExceptionHandler {
 
     }
 
+    /**
+     * @ResponseStatus를 통해 응답코드를 결정할 수 있음
+     * @param e
+     * @return
+     */
+    // @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ServiceResponse<Void> of(Exception e) {
 
